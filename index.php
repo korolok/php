34d6df9bf325316b44974a025ca0a918be1f0ea7
kirill Korolev <antoninakoroleva16@gmail.com>
2022-03-22 15:32:44 +0700

    calc

--- index.php
+++ index.php
@@ -2,7 +2,20 @@
 	header('Content-Type: text/html; charset=utf-8');
 	$ch1 = $_POST['n'];
 	$ch2 = $_POST['s'];
-	$ch3 = $ch1 + $ch2;
-	echo $ch1.'+'.$ch2.'='.$ch3.'<br>';
-	echo '<a href="index.html">Вернуться назад</a>';
+	$dei = $_POST['dei'];
+	switch ($dei) {
+		case '-':
+		echo $ch1 .'-' .$ch2 .'='. $ch1 - $ch2;
+		break;
+		case '+':
+		echo $ch1 .'+' .$ch2 .'='. $ch1 + $ch2;
+		break;
+		case '*':
+		echo $ch1 .'*' .$ch2 .'='. $ch1 * $ch2;
+		break;
+		case '/':
+		echo $ch1 .'/' .$ch2 .'='. $ch1 / $ch2;
+		break;
+	}
+	echo '<br><a href="index.html">Вернуться назад</a>';
 ?>
