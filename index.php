f4aa18fc50f4eeb44ade5781109327212fb5367f
kirill Korolev <antoninakoroleva16@gmail.com>
2022-03-22 15:07:55 +0700

    random

--- index.php
+++ index.php
@@ -2,5 +2,6 @@
 	header('Content-Type: text/html; charset=utf-8');
 	$name = $_POST['n'];
 	$surname = $_POST['s'];
-	echo "Привет ".$name.' '.$surname;
+	echo "Привет ".$name.' '.$surname.'<br>';
+	echo '<a href="index.html">Вернуться назад</a>';
 ?>
