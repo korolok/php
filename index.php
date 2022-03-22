3c8b683955a9944ebce4c3db100ca1a254ca48a8
kirill Korolev <antoninakoroleva16@gmail.com>
2022-03-22 14:42:41 +0700

    random

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 	header('Content-Type: text/html; charset=utf-8');
-	$str1 = 'Привет';
-	$str2 = 'Кирилл';
-	$str3 = '<h1>'.$str1.' '.$str2.'</h1>';
-	echo $str3; 
+	$name = $_GET['n'];
+	echo "Привет ".$name;
 ?>
