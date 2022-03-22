858bc0c654ff14a9f2012c92435b6c99523ea8b2
kirill Korolev <antoninakoroleva16@gmail.com>
2022-03-22 15:13:35 +0700

    sum

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 	header('Content-Type: text/html; charset=utf-8');
-	$name = $_POST['n'];
-	$surname = $_POST['s'];
-	echo "Привет ".$name.' '.$surname.'<br>';
+	$ch1 = $_POST['n'];
+	$ch2 = $_POST['s'];
+	$ch3 = $ch1 + $ch2;
+	echo $ch1.'+'.$ch2.'='.$ch3.'<br>';
 	echo '<a href="index.html">Вернуться назад</a>';
 ?>
