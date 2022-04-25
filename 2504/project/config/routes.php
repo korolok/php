489064532dfc8d546b1e064d5a2228c012e8b038
Ivka <rass219@mail.ru>
2022-04-25 16:40:24 +0700

    задание 93,1

--- 2504/project/config/routes.php
+++ 2504/project/config/routes.php
@@ -2,6 +2,12 @@
 	use \Core\Route;
 	
 	return [
-		new Route('/hello/', 'hello', 'index'), // роут для приветственной страницы, можно удалить
+		new Route('/bye/', 'hello', 'index'),
+		new Route('/my-page1/', 'page', 'show1'),
+		new Route('/my-page2/', 'page', 'show2'),
+		new Route('/act1/', 'test', 'act1'),
+		new Route('/act2/', 'test', 'act2'),
+		new Route('/act3/', 'test', 'act3'),
+
 	];
 	
